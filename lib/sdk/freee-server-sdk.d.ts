630943a789fc144e6b144d80c6ba3fad1212bd4f
ttyniwa <noencrypt@gmail.com>
2024-07-15 13:48:30 +0900

    npm run build

--- lib/sdk/freee-server-sdk.d.ts
+++ lib/sdk/freee-server-sdk.d.ts
@@ -1,19 +1,18 @@
 import { FreeeAPIClient } from './api/freee-api-client';
 import { FreeeFirebaseAuthClient } from './auth/freee-firebase-auth-client';
 import { SDKConfig } from './const/types';
-import { ServiceAccount } from 'firebase-admin';
 import { App } from 'firebase-admin/app';
 export declare class FreeeServerSDK {
     private firebaseAdminApp;
     private apiClient;
     private firebaseAuthClient;
     /**
-     *
-     * @param config
      * @param serviceAccount Cloud Run、App Engine、Cloud Functions などの Google 環境で実行されるアプリケーションではnullを指定することを強くおすすめします。
      *                       https://firebase.google.com/docs/admin/setup?hl=ja#initialize-sdk
      */
-    constructor(config: SDKConfig, serviceAccount: ServiceAccount | null);
+    constructor(config: SDKConfig, serviceAccount: {
+        [key: string]: string;
+    } | null);
     firebaseApp(): App;
     api(): FreeeAPIClient;
     auth(): FreeeFirebaseAuthClient;
